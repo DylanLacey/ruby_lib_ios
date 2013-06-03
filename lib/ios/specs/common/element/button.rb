597ed85b24bfd9aab1c06399e9b463fe007d01f3
bootstraponline <code@bootstraponline.com>
2013-06-03 15:32:18 -0400

    Complete button tests

--- lib/ios/specs/common/element/button.rb
+++ lib/ios/specs/common/element/button.rb
@@ -1,58 +1,55 @@
 # encoding: utf-8
-=begin
+
 describe 'common/element/button' do
   before_first do
     # nav to buttons activity
-    text('App').click
-    text('Activity').click
-    text('Animation').click
+    text('buttons').click
   end
 
   after_last do
-    # nav back to home activity
-    3.times { back; sleep 0.5 }
+    # nav back to start
+    back
   end
 
-  def fade_in
-    'Fade in'
+  def gray
+    'Gray'
   end
 
   t 'button' do
     # by index
-    button(0).name.must_equal fade_in
+    button(1).name.must_equal gray
 
     # by name contains
-    button('ade').name.must_equal fade_in
+    button('ray').name.must_equal gray
   end
 
   t 'buttons' do
-    exp = ['Fade in', 'Zoom in', 'Modern fade in', 'Modern zoom in', 'Scale up', 'Thumbnail zoom']
+    exp = ['Back', 'Gray', 'Right pointing arrow', 'Rounded', 'More info', 'More info', 'More info', 'Add contact']
     buttons.must_equal exp
   end
 
   t 'first_button' do
-    first_button.name.must_equal fade_in
+    first_button.name.must_equal 'Back'
   end
 
   t 'last_button' do
-    last_button.name.must_equal 'Thumbnail zoom'
+    last_button.name.must_equal 'Add contact'
   end
 
   t 'button_exact' do
-    button_exact(fade_in).name.must_equal fade_in
+    button_exact(gray).name.must_equal gray
   end
 
   t 'buttons_exact' do
-    buttons_exact(fade_in).first.name.must_equal fade_in
+    buttons_exact(gray).first.name.must_equal gray
   end
 
   t 'e_buttons' do
-    e_buttons.length.must_equal 6
+    e_buttons.length.must_equal 8
   end
 
   t 'button_num' do
     # select the second button containing 'in'
-    button_num('in', 2).name.must_equal 'Zoom in'
+    button_num('in', 2).name.must_equal 'More info'
   end
 end
-=end
