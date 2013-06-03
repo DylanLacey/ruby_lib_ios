8af78ee844b82ef02763b76fce415c949f286db9
bootstraponline <code@bootstraponline.com>
2013-06-03 15:09:12 -0400

    Complete generic tests

--- lib/ios/specs/ios/element/generic.rb
+++ lib/ios/specs/ios/element/generic.rb
@@ -1,34 +1,31 @@
 # encoding: utf-8
 describe 'ios/element/generic' do
-  t 'first_ele_js' do
 
-  end
-
-  t 'all_ele_js' do
-
+  def exp_text
+    'TextFields, Uses of UITextField'
   end
 
   t 'find' do
-
+    find('text').text.must_equal exp_text
   end
 
   t 'finds' do
-
+    finds('text').first.text.must_equal exp_text
   end
 
   t 'text' do
-
+    text('text').text.must_equal exp_text
   end
 
   t 'texts' do
-
+    texts('text').first.text.must_equal exp_text
   end
 
   t 'name' do
-
+    name('text').name.must_equal exp_text
   end
 
   t 'names' do
-
+    names('text').first.name.must_equal exp_text
   end
 end
